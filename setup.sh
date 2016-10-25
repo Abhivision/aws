ae51e9439069d0324bd34759c863d63e87cd52d5
abhinav_kumar_thakur <athakur@thoughtworks.com>
2016-10-25 22:09:37 +0530

    working files

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
+docker rmi -f aws/web:v0
+docker rmi -f aws/sentiment:v0
 docker build -t aws/web:v0 ./web/
 docker build -t aws/sentiment:v0 ./sentiment/
+docker-compose run web bash app/pg.sh
+docker-compose run sentiment  python robo_app/sentiment.py
