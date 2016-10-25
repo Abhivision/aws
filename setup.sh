7022b36361f7d312b9a852073fecb817ece3fd87
abhinav_kumar_thakur <athakur@thoughtworks.com>
2016-10-25 19:56:06 +0530

    abhivision changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,2 @@
 docker build -t aws/web:v0 ./web/
 docker build -t aws/sentiment:v0 ./sentiment/
-docker-compose run web app/db.sh
-docker-compose run sentiment python roboapp/sentiment.py
-docker-compose up
