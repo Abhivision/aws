b06b51ed376de6737f5ab9c1dc02c0cc30bd73e4
Abhinav Kumar Thakur <Abhivision@users.noreply.github.com>
2016-11-10 13:26:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
-docker rmi -f aws/web:v0 #delete pre-existing build
-docker rmi -f aws/sentiment:v0 #delete pre-existing build
-docker build -t aws/web:v0 ./web/ #create build with latest push
-docker build -t aws/sentiment:v0 ./sentiment/ #create build with latest push
+docker rm -f `docker ps --no-trunc -aq`
+docker rmi -f `docker images --no-trunc -aq`
 docker-compose up
 docker-compose run web bash app/pg.sh #create database with price values
 docker-compose run sentiment  python robo_app/sentiment.py #add news to the database 
