fd1b4685b5f901a0e6f1bf74d901797b83bb9529
Abhinav Kumar Thakur <Abhivision@users.noreply.github.com>
2016-11-11 11:47:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 docker rm -f `docker ps --no-trunc -aq`
 docker rmi -f `docker images --no-trunc -aq`
 docker-compose up
-docker-compose run web bash app/pg.sh #create database with price values
-docker-compose run sentiment  python robo_app/sentiment.py #add news to the database 
